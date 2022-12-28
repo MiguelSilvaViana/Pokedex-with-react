c90b3583d8b99ddc449cbf9543af4ce8dea9d0e1
Miguel Silva <miguelsilvavianaa@gmail.com>
2022-12-28 19:59:41 -0300

    update in the podex project, adding features to save your favorite pokemon and save in localStorage these values, also improving the css and adding new features like the searchbar running now

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,114 @@
-import React, { useEffect, useState } from 'react'
-import './App.css'
-import Navbar from './components/Navbar'
-import Searchbar from './components/Searchbar'
-import Pokedex from './components/Pokedex'
-import {getPokemons, getPokemonData} from './api'
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Searchbar from "./components/Searchbar";
+import Pokedex from "./components/Pokedex";
+import { getPokemons, getPokemonData, searchPokemon } from "./api";
+import { FavoriteProvider } from "./contexts/favoritesContext";
 
+const favoritesKey = "f";
 
 function App() {
-
+  const [page, setPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
   const [loading, setLoading] = useState(false);
+  const [notFound, setNotFound] = useState(false);
   const [pokemons, setPokemons] = useState([]);
+  const [favorites, setFavorites] = useState([]);
 
+  const itensPerPage = 25;
 
   const fetchPokemons = async () => {
     try {
-      setLoading(true)
-      const data = await getPokemons();
+      setLoading(true);
+      setNotFound(false);
+      const data = await getPokemons(itensPerPage, itensPerPage * page);
       const promises = data.results.map(async (pokemon) => {
-        return await getPokemonData(pokemon.url)
-      })
+        return await getPokemonData(pokemon.url);
+      });
       const results = await Promise.all(promises); // result the all promises
-      setPokemons(results)
-      setLoading(false)
+      setPokemons(results);
+      setLoading(false);
+      setTotalPages(Math.ceil(data.count / itensPerPage));
     } catch (error) {
-      console.log("fetchPokemons error", error)
+      console.log("fetchPokemons error", error);
     }
-  }
+  };
+
+  // add in local storage
+  const loadFavoritePokemons = () => {
+    const pokemons =
+      JSON.parse(window.localStorage.getItem(favoritesKey)) || []; // create key
+    setFavorites(pokemons);
+  };
 
   useEffect(() => {
-    console.log('carregou use')
+    loadFavoritePokemons();
+  }, []);
+
+  useEffect(() => {
     fetchPokemons();
-  }, [])
+  }, [page]);
+
+  const updateFavoritePokemons = (name) => {
+    const updateFavorites = [...favorites];
+    const favoriteIndex = favorites.indexOf(name);
+
+    if (favoriteIndex >= 0) {
+      updateFavorites.splice(favoriteIndex);
+    } else {
+      updateFavorites.push(name);
+    }
+
+    window.localStorage.setItem(favoritesKey, JSON.stringify(updateFavorites));
+    setFavorites(updateFavorites);
+  };
+
+  const onSearchHandler = async (pokemon) => {
+    if (!pokemon) {
+      return fetchPokemons(); // get all pokemons
+    }
+
+    setLoading(true);
+    setNotFound(false);
+
+    const result = await searchPokemon(pokemon);
+
+    if (!result) {
+      setNotFound(true);
+    } else {
+      setPokemons([result]); // show pokemons in result
+      setPage(0)
+      setTotalPages(1)
+    }
+    setLoading(false);
+  };
 
   return (
-    <div>
-     <Navbar />
-     <Searchbar/>
-     <Pokedex pokemons={pokemons} loading={loading}/>
-    </div>
-  )
+    <FavoriteProvider
+      value={{
+        favoritePokemons: favorites,
+        updateFavoritePokemons: updateFavoritePokemons,
+      }}
+    >
+      <div>
+        <Navbar />
+        <Searchbar onSearch={onSearchHandler} />
+
+        {notFound ? (
+          <div className="not-found-text">Pokemon Not Found</div>
+        ) : (
+          <Pokedex
+            pokemons={pokemons}
+            loading={loading}
+            page={page}
+            setPage={setPage}
+            totalPages={totalPages}
+          />
+        )}
+      </div>
+    </FavoriteProvider>
+  );
 }
 
-export default App
+export default App;
